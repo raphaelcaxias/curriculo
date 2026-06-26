0657d2a311d225fb0b8c2c99f5c39b0e99b866cf
raphaelcaxias <pires.xx@gmail.com>
2026-06-26 11:49:44 -0300

    Add teste_projects.py file

---
+++ teste_projects.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Teste de Projetos")
+
+st.write("### Projeto 1")
+st.write("Descrição do projeto")
+st.write("[Link](https://github.com)")
+
+st.write("### Projeto 2")
+st.write("Descrição do projeto")
+st.write("[Link](https://github.com)")
